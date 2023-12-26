11fd4b78036435e3c57339f8bf5a0748970a2339
kaungpyae1200 <kaungpyae120045@gmail.com>
2023-12-26 21:25:31 +0630

    update file

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="mb-10 mt-10 font-heading drop-shadow-lg ">
+      <h1 className="text-3xl font-bold text-green-300 w-auto text-center  shadow-lg shadow-cyan-500/50 ">
+        MMS Solutions
+      </h1>
+      <h4 className="text-xl text-green-300 ">Invoice Applications</h4>
+    </header>
+  );
+};
+
+export default Header;
